e5b365c7a3c1349a366e07f18f36dba6a60d9c89
jiyolla <jiyolla0919@gmail.com>
2021-05-25 03:05:42 +0000

    replaced get_server with http

--- client.py
+++ client.py
@@ -6,73 +6,84 @@
 # 2. Generate Request
 #     Sophiscated and Controlled periodic generation
 
-import socket
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import json
 import pickle
 from datetime import datetime, timedelta
 import random
 import concurrent.futures
+import requests
+import imagesize
 # import argparse
 
 
-class Client:
-    def __init__(self, region):
-        self.region = region
-
-
 class Request:
-    def __init__(self, image_path, accuracy, time):
+    def __init__(self, client_region, image_path, accuracy, time):
         self.image_path = image_path
         self.accuracy = accuracy
         self.time = time
+        self.image_size = imagesize.get(image_path)
+        self.timestamps = {
+            'Created': datetime.now(),
+            'Allocated': None,
+            'Served': None
+        }
+
+    def set_allocated(self):
+        self.timestamps['Allocated'] = datetime.now()
+
+    def set_served(self):
+        self.timestamps['Served'] = datetime.now()
 
 
-def generate_request(client, request):
-    with open(request.image_path, 'rb') as f:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            server_address = 'localhost', 64000
-            # server_address = '', 0
-            s.connect(server_address)
+def send_request(request):
+    # 1. Send request meta-data to load balancer to get serving server address
+    # 2. Send actual request to serving server
+    
+    res = requests.get('http://localhost:8000')
+    server = json.loads(res.text)
+    request.set_allocated()
+        
+    # Pre-processing based on the model.
+    # Maybe it should be done the server side. There are pros and cons.
+    
+    # Should be modfied to adapt to the model
+    img = keras.preprocessing.image.load_img(request.image_path, target_size=[224, 224])
+    x = keras.preprocessing.image.img_to_array(img)
+    x = keras.applications.mobilenet.preprocess_input(x[tf.newaxis, ...])
+    
+    
+    data = json.dumps({"signature_name": "serving_default", "instances": x.tolist()})
+    headers = {"content-type": "application/json"}
+    json_response = requests.post(f'http://{server["address"]}/v1/models/{server["model"]}:predict', data=data, headers=headers)
+    predictions = json.loads(json_response.text)
+    response = keras.applications.densenet.decode_predictions(np.array(predictions['predictions']))#[0][0][0]
+    request.set_served()
 
-            image = f.read()
-            meta_info = {
-                'image_id': request.image_path[-28:-5],
-                'image_size': len(image),
-                'requirement': {
-                    'accuracy': request.accuracy,
-                    'time': request.time
-                },
-                'timestamps': {
-                    'created': datetime.now(),
-                    'accepted': None,
-                    'served': None
-                },
-                'initial_server': 0
-            }
-
-            # A custom protocol
-            # [4 bytes indicating image size] + [image] + [meta-info]
-            s.sendall(len(image).to_bytes(4, 'big'))
-            s.sendall(image)
-            s.sendall(pickle.dumps(meta_info))
+    return server['region'], response, request
 
 
 def main():
     # parser = argparse.ArgumentParser()
     # parser.add_argument('-t', '--test', help='Perform local test', action='store_true')
     # args = parser.parse_args()
+    
+    num_req = 10
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        for _ in range(3):
-            region = random.randrange(5)
-            client = Client(region)
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_req) as executor:
+        threads = []
+        for _ in range(num_req):
+            client_region = random.randrange(5)
             image_number = random.randint(1, 100)
             image_path = f'data/image/ILSVRC2012_val_{str(image_number).zfill(8)}.JPEG'
             accuracy = random.uniform(0.5, 1)
             time = timedelta(seconds=random.randint(1, 10))
-            request = Request(image_path, accuracy, time)
+            request = Request(client_region, image_path, accuracy, time)
+            threads.append(executor.submit(send_request, request))
 
-            future = executor.submit(generate_request, client, request)
+        for future in concurrent.futures.as_completed(threads):
             print(future.result())
 
 
--- loadbalancer.py
+++ loadbalancer.py
@@ -13,11 +13,11 @@
 
 from tensorflow import keras
 import tensorflow as tf
+import numpy as np
 import requests
 import socket
 import threading
 import socketserver
-import numpy as np
 from PIL import Image
 import io
 import pickle
@@ -28,10 +28,11 @@ import evaluater
 
 server_states = []
 
-def recvall(socket):
+
+def recvall(s):
     fragments = []
     while True:
-        chunk = socket.recv(1024)
+        chunk = s.recv(1024)
         if not chunk:
             break
         fragments.append(chunk)
@@ -39,66 +40,18 @@ def recvall(socket):
 
 
 class ClientRequestHandler(socketserver.BaseRequestHandler):
-    # Some part of this code should be moved to load_balancer.py
-    # The emulation part stays
+    # Return the serving and model.
 
     def handle(self):
-        data = recvall(self.request)
-
-        # A custom protocol
-        # [4 bytes indicating image size] + [image] + [meta-info]
-        image_header_size = 4
-        # First 4 bytes as the image size
-        image_size = int.from_bytes(data[:image_header_size], 'big')
-        # Load image
-        image = data[image_header_size:image_size + image_header_size]
-        # Load meta-info
-        meta_info = pickle.loads(data[image_size + image_header_size:])
-        meta_info['timestamps']['accepted'] = datetime.now()
+        request = pickle.loads(recvall(self.request))
+        print(request)
         
-        print(meta_info)
-
-        # Send meta_info and server states to DRL
-        # askDRL(meta_info, server_states)
+        # Some DRL decision
         pass
-
-        # ClientRequestHandler ENDS here
-        # Receiving DRL's response/action could be done in other section
-        # Codes below just show that jetson-nano is working
-        # Should be moved into other parts
-        example_drl_response = {
-            'service_address': '222.111.222.238:8501',
-            'service_model': 'MobileNet'
-        }
-        
-        # Preprocessing. It should be moved to serving server.
-        # MODIFY the pretrained keras models to include the preprocessing.
-        # img = keras.preprocessing.image.load_img(file_path, target_size=[224, 224])
-        img = Image.open(io.BytesIO(image)).resize((224, 224))
-        x = keras.preprocessing.image.img_to_array(img)
-        x = keras.applications.mobilenet.preprocess_input(x[tf.newaxis, ...])
-        data = json.dumps({"signature_name": "serving_default", "instances": x.tolist()})
-        
-        # Actual request. It should either be moved to client side or load balancer side.
-        # If load balancer is an edge server then it's probably ok to make actual request in load balancer
-        headers = {"content-type": "application/json"}
-        json_response = requests.post('http://' + example_drl_response['service_address']
-                                      + '/v1/models/'
-                                      + example_drl_response['service_model']
-                                      + ':predict', data=data, headers=headers)
-        predictions = json.loads(json_response.text)
-        meta_info['timestamps']['served'] = datetime.now()
-        # For debugging
-        print(keras.applications.mobilenet.decode_predictions(np.array(predictions['predictions'])))
-
-        meta_info['timestamps']['served'] = datetime.now()
-        response = keras.applications.densenet.decode_predictions(np.array(predictions['predictions']))[0][0][0]
-        
-        # call evaluater to rate the response
-        is_timely, is_success = evaluater.evaluate(meta_info, response)
-
-        # feed back to DRL
-        pass
+    
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.send
+    
 
 
 class ServerMonitorHandler(socketserver.BaseRequestHandler):
