1c3da9043560402021474da3ae84c7d651f78700
jiyolla <jiyolla0919@gmail.com>
2021-05-27 23:45:22 +0000

    drl.py logic completed. Debugging yet to be done.

--- drl.py
+++ drl.py
@@ -1,4 +1,6 @@
-from multiprocessing import Process, Pipe
+import tensorflow as tf
+import numpy as np
+from multiprocessing import Process, Pipe, Queue
 import loadbalancer
 import servermonitor
 import evaluater
@@ -7,6 +9,7 @@ import threading
 import serverstate
 import request
 from collections import deque
+import random
 
 
 # Currently cannot adatp to change in #server/#model
@@ -17,12 +20,27 @@ class DRL:
         # Hpyerparemeter settings
         self.batch_size = 100
         self.observation_interval = 0.1
+        self.explore_chance = 0.7
+        self.final_explore_chance = 0.01
+        self.explore_chance_decay = 0.995
+        self.discount_factor = 0.99
 
         self.request_queue = deque()
+        self.memory = []
         self.server_states = {}
+        self.action_space = 9 * 3 + 1
+        self.state_space = 4 + 5 * 3
         self.pipe_to_loadbalancer = pipe_to_loadbalancer
         self.pipe_to_servermonitor = pipe_to_servermonitor
         self.pipe_to_evaluater = pipe_to_evaluater
+
+    def build_model(self):
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(units=64, activation='relu', input_dim=self.state_space))
+        model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+        model.add(tf.keras.layers.Dense(units=self.state_space, activation='linear'))
+        model.compile(loss=tf.keras.losses.Huber, optimizer=tf.keras.optimizers.Adam)
+        return model
 
     def reward_function(result):
         if sum(result) == 2:
@@ -33,10 +51,32 @@ class DRL:
             reward = -20
         return reward
 
+    def batch_train(self, model, ret):
+        batch = self.memory[:self.batch_size]
+        with threading.Lock():
+            self.memory = self.memory[self.batch_size:]
+
+        X = []
+        Y = []
+        for state, action, next_state, reward in batch:
+            reward = reward + self.discount_factor * np.amax(self.model.predict(next_state)[0])
+            target = self.model.predict(state)[0]
+            target[action] = reward
+            X.append(state)
+            Y.append(target)
+        X = np.array(X)
+        Y = np.array(Y)
+        model.fit(X, Y, epochs=1, verbose=0)
+        ret.put(model)
+
     def run_and_train(self):
+        model = self.build_model()
+        new_model = None
+        p_train = None
+        ret = Queue()
         while True:
             prev_state = serverstate.ServerState.empty_state()
-            prev_action = None
+            prev_action = 0
             for _ in range(self.batch_size):
                 time.sleep(self.observation_interval)
 
@@ -55,21 +95,33 @@ class DRL:
                 else:
                     req_state = self.request_queue.popleft().to_state()
                 svr_state = [state_el for server_state in self.server_states.values() for state_el in server_state.to_state()]
-                state = req_state + svr_state
-                # print(state)
-                # action = model.predict(state) + some randomness
+                state = np.array(req_state + svr_state)
+
+                if random.random() < self.explore_chance:
+                    action = random.randrange(self.action_space)
+                else:
+                    action = model.predict(state)
+                self.explore_chance *= self.explore_chance_decay
 
                 # Get reward from evaluater
                 reward = 0
                 if self.pipe_to_evaluater.poll():
-                    reward = reward_function(self.pipe_to_evaluater.recv())
+                    reward = self.reward_function(self.pipe_to_evaluater.recv())
                 # print(reward)
 
-                # memory.append(prev_state, prev_action, state, reward)
-                #prev_state = state
-                #prev_acton = action
+                with threading.Lock():
+                    self.memory.append(prev_state, prev_action, state, reward)
+                prev_state = state
+                prev_action = action
+
             # start a thread to batch training a new model
             # replace current model with new model upon completion
+            if p_train is not None:
+                model = ret.get()
+                p_train.join()
+            new_model = model
+            p_train = Process(target=self.batch_train, args=(new_model, ret))
+            p_train.start()
 
 
 def main():
