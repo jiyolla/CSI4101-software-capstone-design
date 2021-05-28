f02eeb89eed83f1d0f97395c40ecd970a1be92c9
jiyolla <jiyolla0919@gmail.com>
2021-05-29 05:03:04 +0900

    model deployment fixed

--- service_side/export_keras_pretrained.py
+++ service_side/export_keras_pretrained.py
@@ -1,19 +1,9 @@
 import tensorflow as tf
 
-pretrained_model = tf.keras.applications.Xception()
-tf.saved_model.save(pretrained_model, './models/Xception/1')
+models = ['Xception', 'MobileNetV2', 'DenseNet121', 'DenseNet169', 'DenseNet201', 'NASNetMobile']
 
-pretrained_model = tf.keras.applications.MobileNetV2()
-tf.saved_model.save(pretrained_model, './models/MobileNetV2/1')
+for model in models:
+    loaded_model = eval(f'tf.keras.applications.{model}()')
+    tf.saved_model.save(loaded_model, f'./models/{model}/1')
+    loaded_model = None
 
-pretrained_model = tf.keras.applications.DenseNet121()
-tf.saved_model.save(pretrained_model, './DenseNet121/1')
-
-pretrained_model = tf.keras.applications.DenseNet169()
-tf.saved_model.save(pretrained_model, './DenseNet169/1')
-
-pretrained_model = tf.keras.applications.DenseNet201()
-tf.saved_model.save(pretrained_model, './DenseNet201/1')
-
-pretrained_model = tf.keras.applications.NASNetMobile()
-tf.saved_model.save(pretrained_model, './NASNetMobile/1')
