5f7169661aae0003e84d6febede75a9a4fbbe9e6
jiyolla <jiyolla@JetsonNano.localdomain>
2021-05-29 21:38:53 +0900

    bug fixed

--- service_side/deploy_models.py
+++ service_side/deploy_models.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import tensorflow as tf
+# import tensorflow as tf
 
 sys.path.append(os.path.join(sys.path[0], '..'))
 from common import available_models
@@ -10,15 +10,15 @@ def main():
     with open('models.config', 'w') as f:
         f.write('model_config_list {\n')
         for model in available_models.lst:
-            print(f'Loading {model}...')
-            loaded_model = eval(f'tf.keras.applications.{model}')
-            tf.saved_model.save(loaded_model(), f'./models/{model}/1')
-            print(f'{model} loaded.')
-            f.write('    config {\n')
-            f.write(f'        name: {model}\n')
-            f.write(f'        base_path: "/service/models/{model}/"\n')
-            f.write('model_platform: "tensorflow"\n')
-            f.write('    }\n')
+            #print(f'Loading {model}...')
+            #loaded_model = eval(f'tf.keras.applications.{model}')
+            #tf.saved_model.save(loaded_model(), f'./models/{model}/1')
+            #print(f'{model} loaded.')
+            f.write('  config {\n')
+            f.write(f'    name: \'{model}\'\n')
+            f.write(f'    base_path: \'/service/models/{model}/\'\n')
+            f.write('    model_platform: \'tensorflow\'\n')
+            f.write('  }\n')
         f.write('}')
 
 
